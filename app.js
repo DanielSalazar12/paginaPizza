8dc9016f17b1123e7caa86ada30ad0619d8945a8
Daniel <salazarhjuand@iezaragoza.net>
2024-08-15 11:37:54 -0500

    avance pagina pizza del dia2 en clase

---
+++ app.js
@@ -0,0 +1,40 @@
+let cantidadPizza1=0;
+let cantidadPizza2=0;
+let cantidadPizza3=0;
+let cantidadPizza4=0;
+let cantidadPizza5=0;
+
+function pizza1() {
+  cantidadPizza1 = document.getElementById("cantidadPizza1").value;
+  console.log(cantidadPizza1);
+
+  alert("khtuerngfuh");
+}
+function pizza2() {
+  cantidadPizza2 = document.getElementById("cantidadPizza2").value;
+  console.log(cantidadPizza2);
+  
+  alert("khtuerngfuh");
+}
+function pizza3() {
+    cantidadPizza3 = document.getElementById("cantidadPizza3").value;
+    console.log(cantidadPizza3);
+    
+    alert("khtuerngfuh");
+}
+  function pizza4() {
+    cantidadPizza4 = document.getElementById("cantidadPizza4").value;
+    console.log(cantidadPizza4);
+    
+    alert("khtuerngfuh");
+}
+function pizza5() {
+    cantidadPizza5 = document.getElementById("cantidadPizza5").value;
+    console.log(cantidadPizza5);
+    
+    alert("khtuerngfuh");
+  }
+  
+  function calcularPago(){
+    
+  }
